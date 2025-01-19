62d6ef3a3243788e815496a27a0056367dacfff7
nakira0x4D <maxime.loukhal@gmail.com>
2025-01-19 16:15:01 +0100

    Add test project to solution

---
+++ Lkhsoft.Collections.Test/RedBlackTreeTests.cs
@@ -0,0 +1,15 @@
+﻿namespace Lkhsoft.Collections.Test;
+
+public class RedBlackTreeTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
